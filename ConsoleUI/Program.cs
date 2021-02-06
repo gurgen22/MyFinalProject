2df654a638e24800f310048b31534b59ec253819
gurgenk022 <gurgenk022@gmail.com>
2021-02-06 23:36:06 +0300

    6 Şubat 2. Mola

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,14 +9,22 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            // ProductText();
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+
+        }
+
+        private static void ProductText()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetByUnitPrice(40,100))
+            foreach (var product in productManager.GetByUnitPrice(40, 100))
             {
                 Console.WriteLine(product.ProductName);
             }
-
-            
-
         }
     }
 }
