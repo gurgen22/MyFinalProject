9f589aaf36dc8ef4e4b9a12e484ecd459f40109f
gurgenk022 <gurgenk022@gmail.com>
2021-03-13 21:47:38 +0300

    13 Mart Commit

---
+++ WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,36 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        
+        private ICategoryService _categorytService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categorytService = categoryService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            
+            var result = _categorytService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+
+}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -41,8 +41,8 @@ namespace WebAPI
             //services.AddSingleton<IProductService,ProductManager>();
             //services.AddSingleton<IProductDal, EfProductDal>();
 
-            
 
+            services.AddCors();
 
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
@@ -74,6 +74,7 @@ namespace WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
             app.UseHttpsRedirection();
 
 
